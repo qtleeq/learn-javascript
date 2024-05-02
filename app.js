800d65eb808875d2a69122f6e22b7757cc60bb9c
Taylor D. Lee <taylor.dennis.lee@gmail.com>
2024-05-02 09:13:41 -0500

    Initial

---
+++ app.js
@@ -0,0 +1,73 @@
+// var my_name = document.getElementById("greeting").innerHTML;
+
+// document.getElementById("greeting").style.display = "none";
+
+// document.getElementById("greeting").style.display = "block";
+
+function greeting() {
+  document.getElementById("greeting").innerHTML = "I'm a boss times 10";
+}
+
+// greeting();
+
+let a, b, c; // Declare 3 variables
+a = 5;
+b = 6;
+c = a + b;
+console.log(c);
+
+
+
+let firstName;
+let age;
+let city;
+firstName = "Taylor";
+age = 36;
+city = "Madison";
+
+console.log(firstName + " smiles");
+
+console.log(`My name is ${firstName} and I'm from ${city}`);
+
+function myFunction() {
+  document.getElementById("greeting").innerHTML = "Hello Taylor";
+
+  document.getElementById("greeting").style.fontSize = "100px";
+
+  document.getElementById("greeting").style.backgroundColor = "yellow";
+}
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+// let x = true;
+// let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+document.getElementById('off').innerHTML = 'Off'
+document.getElementById('on').innerHTML = 'On'
+document.getElementById('off').style.padding = "100px";
+document.getElementById('on').style.padding = "100px";
+document.getElementById('off').style.borderRadius = "20px";
+document.getElementById('on').style.borderRadius = "20px";
+document.getElementById('on').style.borderWidth = "20px";
+document.getElementById('off').style.borderWidth = "20px";
+
+
+document.getElementById('off').style.backgroundColor = "red";
+document.getElementById('on').style.backgroundColor = "blue";
+
